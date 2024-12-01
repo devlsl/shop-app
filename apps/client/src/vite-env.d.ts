b89542c7ed2f269a256455cc1820f276b4e56cb6
devlsl <vslozhen@gmail.com>
2024-12-01 17:24:30 +0300

    deleted comments from vite-env.ts

--- apps/client/src/vite-env.d.ts
+++ apps/client/src/vite-env.d.ts
@@ -1,19 +1,5 @@
 /// <reference types="vite/client" />
 
-// interface ImportMetaEnv {
-//     readonly STATIC_SERVER_HOSTNAME: string;
-//     readonly STATIC_SERVER_PORT: string;
-
-//     readonly SERVER_HOSTNAME: string;
-//     readonly SERVER_PORT: string;
-
-//     readonly CLIENT_HOSTNAME: string;
-//     readonly CLIENT_PORT: string;
-// }
-
-// interface ImportMeta {
-//     readonly env: ImportMetaEnv;
-// }
 declare const __APP_ENV__: Record<
     'SERVER_HOSTNAME' | 'CLIENT_URL_PARAMS_KEY',
     string
